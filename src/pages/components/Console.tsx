de8df146813ceef7a039529510f18d02958f67d0
Tan Lip Wei <tlipwei@gmail.com>
2023-03-04 14:06:29 +0800

    Refactor to use react tsx

--- src/pages/components/Console.tsx
+++ src/pages/components/Console.tsx
@@ -1,12 +1,13 @@
-import { FC, RefObject } from 'react';
+import { Children, FC, RefObject } from 'react';
 
 interface Props {
   consoleRef: RefObject<HTMLDivElement>;
+  html: (() => JSX.Element)[];
 }
 
 // eslint-disable-next-line arrow-body-style
-const Console: FC<Props> = ({ consoleRef }) => {
-  return <div ref={consoleRef} />;
+const Console: FC<Props> = ({ consoleRef, html }) => {
+  return <div ref={consoleRef}>{html.length && Children.toArray(html.map((el) => el()))}</div>;
 };
 
 export default Console;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,13 @@ import { Source_Code_Pro as SourceCodePro } from 'next/font/google';
 import { useState, useRef, useEffect } from 'react';
 
 import {
-  iterateArrayWithDelay,
-  helpText,
   banner,
   aboutMe,
   education,
   socials,
   technologies,
+  helpText,
+  unknown,
 } from '@/lib/console';
 
 import About from './components/About';
@@ -20,70 +20,87 @@ const sourceCodePro = SourceCodePro({ subsets: ['latin'], weight: '400' });
 
 export default function Home() {
   const [input, setInput] = useState('');
+  const [html, setHtml] = useState<(() => JSX.Element)[]>([]);
+  const [content, setContent] = useState<any[]>([]);
+  const [idx, setIdx] = useState(0);
   const consoleRef = useRef<HTMLDivElement>(null);
   const inputContainer = useRef<HTMLDivElement>(null);
+  const testRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     if (consoleRef.current) {
-      iterateArrayWithDelay(80, consoleRef.current, banner, inputContainer);
+      setContent(banner);
     }
   }, []);
+
+  // eslint-disable-next-line consistent-return
+  useEffect(() => {
+    if (idx < content.length) {
+      const timer = setTimeout(() => {
+        setHtml((prev) => [...prev, content[idx]]);
+        setIdx((prev) => prev + 1);
+      }, 80);
+      return () => {
+        const height = inputContainer.current?.offsetTop;
+        testRef.current?.scroll({ top: height, behavior: 'smooth' });
+        return clearTimeout(timer);
+      };
+    }
+
+    setIdx(0);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [html, content]);
 
   const onEnter = () => {
     if (consoleRef.current) {
       const nextEl = document.createElement('p');
       nextEl.innerHTML = input;
       nextEl.id = 'command';
-      const newEl = consoleRef.current.appendChild(nextEl);
+      consoleRef.current.appendChild(nextEl);
       setInput('');
+      setIdx(0);
+      setContent([]);
       switch (input) {
         case 'help': {
-          iterateArrayWithDelay(80, newEl, helpText, inputContainer);
+          setContent([...helpText]);
           break;
         }
         case 'aboutme': {
-          iterateArrayWithDelay(80, newEl, aboutMe, inputContainer);
+          setContent([...aboutMe]);
           break;
         }
         case 'clear': {
           consoleRef.current.innerHTML = '';
-          iterateArrayWithDelay(80, consoleRef.current, banner, inputContainer);
+          setContent([...banner]);
           break;
         }
         case 'banner': {
-          iterateArrayWithDelay(80, newEl, banner, inputContainer);
+          setContent([...banner]);
           break;
         }
         case 'education': {
-          iterateArrayWithDelay(80, newEl, education, inputContainer);
+          setContent([...education]);
           break;
         }
         case 'technologies': {
-          iterateArrayWithDelay(80, newEl, technologies, inputContainer);
+          setContent([...technologies]);
           break;
         }
         case 'socials': {
-          iterateArrayWithDelay(80, newEl, socials, inputContainer);
+          setContent([...socials]);
           break;
         }
         default: {
-          iterateArrayWithDelay(
-            0,
-            newEl,
-            {
-              classForAll: ['content'],
-              content: [{ content: 'Unknown command. Try typing "help"' }],
-            },
-            inputContainer
-          );
+          setContent([...unknown]);
         }
       }
     }
   };
 
   return (
-    <div className="flex justify-center items-center h-screen lg:py-12">
-      <div className="max-w-screen-lg w-full bg-[#073642] overflow-hidden flex flex-col lg:rounded-xl max-h-[888px]">
+    <div className="flex justify-center items-center h-screen py-12">
+      <div className="max-w-screen-lg w-full bg-[#073642] overflow-hidden flex flex-col lg:rounded-xl max-h-full">
         <div className="bg-[#032b36] py-2 px-4 flex gap-x-2">
           <div className="bg-red-400 rounded-full h-4 w-4" />
           <div className="bg-yellow-400 rounded-full h-4 w-4" />
@@ -94,9 +111,10 @@ export default function Home() {
           <Age />
           <hr className="border-[#032b36] my-4" />
           <div
+            ref={testRef}
             className={`${sourceCodePro.className} overflow-scroll flex-grow flex flex-col py-4`}
           >
-            <Console consoleRef={consoleRef} />
+            <Console consoleRef={consoleRef} html={html} />
             <Input
               input={input}
               setInput={setInput}
