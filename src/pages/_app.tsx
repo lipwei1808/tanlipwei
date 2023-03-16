c00ded5a7f06d9d99584f2dbdffa3ded8a49e0d8
Tan Lip Wei <tlipwei@gmail.com>
2023-03-17 00:30:45 +0800

    Added SEO tags and robots

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { Source_Code_Pro as SourceCodePro } from 'next/font/google';
+import Head from 'next/head';
 import 'react-tooltip/dist/react-tooltip.css';
 
 import '@/styles/globals.css';
@@ -16,12 +17,44 @@ const sourceCodePro = SourceCodePro({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ActiveElementProvider>
-      <InputProvider>
-        <main className={sourceCodePro.className}>
-          <Component {...pageProps} />
-        </main>
-      </InputProvider>
-    </ActiveElementProvider>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="locale" content="en-SG" />
+        <meta name="theme-color" content="#000" />
+        <meta
+          name="description"
+          content="NUS Computer Science Undergraduate and Full-Stack Web Developer from Singapore"
+        />
+        <link rel="shortcut icon" sizes="48x48" href="/favicon/favicon.ico`" />
+        <link rel="icon" sizes="192x192" href="/favicon/favicon192.png`" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png`" />
+        <link rel="canonical" href="https://www.tanlipwei.com" />
+        <meta property="og:title" content="Tan Lip Wei" />
+        <meta property="og:site_name" content="Tan Lip Wei" />
+        <meta property="og:locale" content="en-SG" />
+        <meta property="og:url" content="https://www.tanlipwei.com" />
+        <meta
+          property="og:description"
+          content="NUS Computer Science Undergraduate and Full-Stack Web Developer from Singapore"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="/og_image.jpg`" />
+        <meta name="twitter:title" content="Tan Lip Wei" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:url" content="https://www.tanlipwei.com" />
+        <meta
+          name="twitter:description"
+          content="NUS Computer Science Undergraduate and Full-Stack Web Developer from Singapore"
+        />
+      </Head>
+      <ActiveElementProvider>
+        <InputProvider>
+          <main className={sourceCodePro.className}>
+            <Component {...pageProps} />
+          </main>
+        </InputProvider>
+      </ActiveElementProvider>
+    </>
   );
 }
