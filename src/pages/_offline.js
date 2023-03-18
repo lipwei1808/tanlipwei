2fa917c890a354cb19b68e7eea7de642e566b833
Tan Lip Wei <tlipwei@gmail.com>
2023-03-18 19:07:58 +0800

    Updated offline page

--- src/pages/_offline.js
+++ src/pages/_offline.js
@@ -1,3 +1,13 @@
-const Index = () => <div className="bg-black">Hello World</div>;
+import Head from 'next/head';
 
-export default Index;
+const Fallback = () => (
+  <>
+    <Head>
+      <title>next-pwa example</title>
+    </Head>
+    <h1>This is offline fallback page</h1>
+    <h2>When offline, any page route will fallback to this page</h2>
+  </>
+);
+
+export default Fallback;
