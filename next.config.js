462be1607b84f07cceea11ea2367f80cd1bf76b9
Tan Lip Wei <tlipwei@gmail.com>
2023-03-18 18:16:54 +0800

    Add fallback page

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const path = require('path');
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withPWA = require('next-pwa')({
   dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
 });
 
 const nextConfig = withPWA({
